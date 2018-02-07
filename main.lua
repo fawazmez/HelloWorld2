3dfcdd7b49f1b145c19a6cc30ac9765dfb537dfe
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-02-07 13:17:43 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- 
+-- main.lua
+--
+-- Created by : Fawaz Mezher
+-- Created on : Feb 2018
+--
+-- This file prints "Hello World!" tho the console
+-----------------------------------------------------------------------------------------
+
+ print( "Hello, World!")
+ local myText = display.newText( 'Hello, World!' , 200, 200, native.systemfont, 42)
+
+
+
